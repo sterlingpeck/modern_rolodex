c813e2a513d9991313fdc14528889d1f3e1ae7a0
Sterling Peck <peck.sterling@gmail.com>
2022-06-10 09:29:26 -0600

    Some DB, html and css structure

---
+++ server.js
@@ -0,0 +1,13 @@
+const path = require("path");
+const express = require("express");
+const session = require("express-session");
+const exphbs = require("express-handlebars");
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const sequelize = require("./config/connection");
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log("Now listening"));
+});
